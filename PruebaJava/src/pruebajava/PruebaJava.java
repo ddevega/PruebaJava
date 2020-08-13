89098baf9a3d486b05bdc74afa47f752895cff2c
ddevega <dvegrod817@ieshnosmachado.org>
2020-08-13 19:36:56 +0200

    Primer commit con prueba inicial

---
+++ PruebaJava/src/pruebajava/PruebaJava.java
@@ -0,0 +1,14 @@
+
+package pruebajava;
+
+
+public class PruebaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba Git y GitHub");
+    }
+    
+}
